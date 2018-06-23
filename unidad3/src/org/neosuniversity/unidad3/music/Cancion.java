2cc01577acab6c4715b88ef84357edd0b4113e51
hector77 <hector.hidalgo@globant.com>
2018-06-23 12:22:51 -0500

    se agrega codigo fuente para unidad 3 Biblioteca Musica parte 1

---
+++ unidad3/src/org/neosuniversity/unidad3/music/Cancion.java
@@ -0,0 +1,14 @@
+package org.neosuniversity.unidad3.music;
+
+/**
+ * Created by camus on 02/06/2018.
+ */
+public class Cancion {
+
+    private String nombreCancion;
+    private float duracion;
+
+    //crear constructores
+
+    //crear metodos setters y getters
+}
---
+++ unidad3/src/org/neosuniversity/unidad3/music/Disco.java
@@ -0,0 +1,16 @@
+package org.neosuniversity.unidad3.music;
+
+/**
+ * Created by camus on 02/06/2018.
+ */
+public class Disco {
+
+    private String nombreDisco;
+    private int annoLanzamiento;
+    private Cantante cantante;
+    private Cancion[] lstCanciones;
+    private String genero;
+    
+    //crear constructores
+    //crear metodos setters y getters
+}
